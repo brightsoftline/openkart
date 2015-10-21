877019685d6eabdb3f1678572d6ec95c240d1aaf
Ashraf Quadri Syed <emails2ashraf@gmail.com>
2015-10-21 14:13:07 +0530

    new commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/netbean/admin/');
+define('HTTP_CATALOG', 'http://localhost/netbean/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/netbean/admin/');
+define('HTTPS_CATALOG', 'http://localhost/netbean/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/netbean/admin/');
+define('DIR_SYSTEM', 'D:/wamp/www/netbean/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/netbean/admin/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/netbean/admin/view/template/');
+define('DIR_CONFIG', 'D:/wamp/www/netbean/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/netbean/image/');
+define('DIR_CACHE', 'D:/wamp/www/netbean/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/netbean/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/netbean/system/upload/');
+define('DIR_LOGS', 'D:/wamp/www/netbean/system/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/netbean/system/modification/');
+define('DIR_CATALOG', 'D:/wamp/www/netbean/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'netbean');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/netbean/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/netbean/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/netbean/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/netbean/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/netbean/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/netbean/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/netbean/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/netbean/image/');
+define('DIR_CACHE', 'D:/wamp/www/netbean/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/netbean/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/netbean/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/netbean/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/netbean/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'netbean');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
